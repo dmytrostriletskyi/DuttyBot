e0660bec73cf60d5861c95fa0610255a3e035ecf
Dmytryi Strilteskyi <dmytryi.striletskyi@gmail.com>
2016-12-15 00:16:58 +0200

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,24 +1,20 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from bs4 import BeautifulSoup
+
 import dj_database_url
-import postgresql
 import requests
 import psycopg2
 
 import datetime
-import random
 
 import utils.dataProcessing as dataProcessing
 import dataLib 
 import config 
 
 
-
-
 sched = BlockingScheduler()
 
-
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=22, minute=1)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=22)
 def dateApp():
 	print ('Date app running!')
 	db_info = dj_database_url.config(default=config.DBSRC)
@@ -31,35 +27,15 @@ def dateApp():
 	   port=db_info.get('PORT'))
 
 	cursor = connection.cursor()
-
-	cursor.execute("DROP TABLE IF EXISTS dateapp") 
-	cursor.execute("CREATE TABLE IF NOT EXISTS dateapp (id serial primary key, datefirst text, datesecond text, daytoweek text);")
+	cursor.execute("INSERT INTO dateapp (done) VALUES (%s)", ('done', ))
 	
-
-	native = datetime.datetime.now()
-
-	firstDay = native + datetime.timedelta(hours = 2) 
-	lastDay = firstDay + datetime.timedelta(days = 1) 
-
-	dayToWeek = native + datetime.timedelta(hours = 2) 
-	dayToWeek = str(dayToWeek.weekday())
-
-	firstDay = firstDay.strftime("%d.%m")
-	lastDay = lastDay.strftime("%d.%m")
-
-
-	cursor.execute("INSERT INTO dateapp (datefirst, datesecond, daytoweek) VALUES (%s, %s, %s)", (firstDay, lastDay, dayToWeek))
 	connection.commit()
 	connection.close()
 
 
-
-
-	
-
-@sched.scheduled_job('cron', day_of_week=5, hour=3, minute=00)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=22, minute=1)
 def createTable():
-	print ('Create table running!')
+	print ('Create everyday shedule!')
 	db_info = dj_database_url.config(default=config.DBSRC)
 
 	connection = psycopg2.connect(
@@ -71,34 +47,72 @@ def createTable():
 
 	cursor = connection.cursor()
 
-	cursor.execute("DROP TABLE IF EXISTS timetable") 
-	cursor.execute("CREATE TABLE IF NOT EXISTS timetable (id serial primary key, faculty char(29), course char(1), groupa text, dateFirst char(5), schedule text);")
+	cursor.execute("DELETE FROM timetable") 
 	connection.commit()
 
-		for i in range(1):
+	
+	nativeDay = datetime.datetime.now() + datetime.timedelta() 
+	weekday = datetime.datetime.today().weekday()
 
-		nativeDay = datetime.datetime.now() + datetime.timedelta() 
-		weekday = datetime.datetime.today().weekday()
+	firstDay = nativeDay + datetime.timedelta(-weekday) 
+	lastDay = firstDay + datetime.timedelta(6) 
 
-		firstDay = nativeDay + datetime.timedelta(i * 7 - weekday) 
-		lastDay = firstDay + datetime.timedelta(6) 
+	firstDay = firstDay.strftime("%d.%m.%Y")
+	lastDay = lastDay.strftime("%d.%m.%Y")
 
-		firstDay = firstDay.strftime("%d.%m.%Y")
-		lastDay = lastDay.strftime("%d.%m.%Y")
+
+	print(firstDay)
+	print(lastDay)
 		
-		print(firstDay)
-		print(lastDay)
+	for formData in dataLib.dateToDateForm(firstDay, lastDay):
+		r = requests.post(config.url, data = formData)
+		soup = BeautifulSoup(r.text, 'html.parser')
+		dataProcessing.dataProcessing(soup) 
 		
-		for formData in dataLib.dateToDateForm(firstDay, lastDay):
-			r = requests.post(config.url, data = formData)
-			soup = BeautifulSoup(r.text, 'html.parser')
-			dataProcessing.dataProcessing(soup) 
+	connection.commit()
+	connection.close()
+
+
+@sched.scheduled_job('cron', day_of_week=5, hour=22, minute=1)
+def createTable():
+	print ('Create shedule from monday to friday next weak as sunday 00:01 parser!')
+	db_info = dj_database_url.config(default=config.DBSRC)
+
+	connection = psycopg2.connect(
+	   database=db_info.get('NAME'),
+	   user=db_info.get('USER'),
+	   password=db_info.get('PASSWORD'),
+	   host=db_info.get('HOST'),
+	   port=db_info.get('PORT'))
+
+	cursor = connection.cursor()
+
+	cursor.execute("DELETE FROM timetable") 
+	connection.commit()
+
+	
+	nativeDay = datetime.datetime.now() + datetime.timedelta() 
+	weekday = datetime.datetime.today().weekday()
+
+	firstDay = nativeDay + datetime.timedelta(+2) 
+	lastDay = firstDay + datetime.timedelta(6) 
+
+	firstDay = firstDay.strftime("%d.%m.%Y")
+	lastDay = lastDay.strftime("%d.%m.%Y")
+
+	print(firstDay)
+	print(lastDay)
+		
+	for formData in dataLib.dateToDateForm(firstDay, lastDay):
+		r = requests.post(config.url, data = formData)
+		soup = BeautifulSoup(r.text, 'html.parser')
+		dataProcessing.dataProcessing(soup) 
 		
 
 	connection.commit()
 	connection.close()
 
-	
+
 sched.start()
 
 
